4c457d359c6a2013090ac660d903aa0b57217352
huhuiqian <515516132@qq.com>
2019-06-25 17:56:12 +0800

    修复因读取文件失败导致的报错

--- generators/app/client/templates/koa/src/util/ScanControllerFiles.ts
+++ generators/app/client/templates/koa/src/util/ScanControllerFiles.ts
@@ -2,6 +2,9 @@ import * as fs from 'fs';
 
 export function scanControllerFiles(callback?: Function) {
   fs.readdir('src/controller', (err, files) => {
+    if (!files) {
+      return
+    }
     const regex = /^[\w-]+(\.js)$/;
     files.forEach((fileName) => {
       if (regex.test(fileName)) {
